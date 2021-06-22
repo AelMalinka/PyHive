379ae875159ebf52653f81bd0e11035ade82ea9c
Adam Cook <adam.cook@maxar.com>
2021-06-22 10:41:07 -0400

    Update sqlalchemy_hive.py

--- pyhive/sqlalchemy_hive.py
+++ pyhive/sqlalchemy_hive.py
@@ -373,10 +373,7 @@ class HiveDialect(default.DefaultDialect):
         query = 'SHOW TABLES'
         if schema:
             query += ' IN ' + self.identifier_preparer.quote_identifier(schema)
-        return list(row[1] for row in filter(
-            lambda x: not x[-1],
-            [row for row in connection.execute(query)]
-        ))
+        return [row[1] for row in connection.execute(query)]
 
     def do_rollback(self, dbapi_connection):
         # No transactions for Hive
